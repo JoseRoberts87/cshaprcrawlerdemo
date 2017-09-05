64b1d5848db4dcf7919448a46122049a428e2bec
joseroberts87 <webterpr@gmail.com>
2017-09-04 23:52:29 -0400

    testing other urls

--- Program.cs
+++ Program.cs
@@ -12,7 +12,7 @@ namespace ConsoleApplication1
         {
             Console.WriteLine("console line is this");
             HtmlAgilityPack.HtmlWeb web = new HtmlAgilityPack.HtmlWeb();
-            HtmlAgilityPack.HtmlDocument doc = web.Load("https://www.yellowpages.com/providence-ri/sofware?autocorrect=off");
+            HtmlAgilityPack.HtmlDocument doc = web.Load("https://www.amazon.com/ROVA-Flying-Selfie-Drone-Camera/dp/B06WVDJYGD");
             var HeaderNames = doc.DocumentNode
                 .SelectNodes("//a[@class='business-name']").ToList();
             foreach(var item in HeaderNames)
