f9f1acde3b89a3a52c63e5090ef108c2abdf0cf8
joseroberts87 <webterpr@gmail.com>
2017-09-05 20:02:09 -0400

    storing results in variable to write to file

--- Program.cs
+++ Program.cs
@@ -12,12 +12,13 @@ namespace ConsoleApplication1
         static void Main(string[] args)
         {
             SiteCrawler();
-            StoreData();
+            
             Console.WriteLine("finished");
         }
 
         static void SiteCrawler()
         {
+            string myLines = "";
             Console.WriteLine("console line is this");
             HtmlAgilityPack.HtmlWeb web = new HtmlAgilityPack.HtmlWeb();
             HtmlAgilityPack.HtmlDocument doc = web.Load("https://providence.craigslist.org/search/sss?query=supra&sort=rel");
@@ -26,18 +27,21 @@ namespace ConsoleApplication1
             foreach (var item in HeaderNames)
             {
                 Console.WriteLine(item.InnerHtml);
+                myLines += item.InnerHtml;
+                
             }
+            StoreData(myLines);
         }
-        
-        static void StoreData()
+
+        static void StoreData(string writethis)
         {
             string lines = "save me";
             string path = Directory.GetCurrentDirectory();
-            string mypath = Environment.CurrentDirectory + @"\ ..";
-            Console.WriteLine(path);
-            Console.WriteLine(mypath);
+            string mypath = @".. \" + path;
+            //Console.WriteLine(path);
+            //Console.WriteLine(mypath);
             System.IO.StreamWriter file = new System.IO.StreamWriter("C:\\Users\\rober\\Documents\\Visual Studio 2015\\Projects\\ConsoleApplication1\\myfile.txt");
-            file.WriteLine(lines);
+            file.WriteLine(writethis);
             file.Close();
         }
     }
